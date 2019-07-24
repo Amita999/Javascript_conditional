9ec606d51e6a4d530c104665b4cc863039df8fb3
amita.jethani <amitajethani@gmail.com>
2019-07-24 17:26:00 +0530

    Simple if else

---
+++ conditional.js
@@ -0,0 +1,13 @@
+function isTrue(wasTrue)
+{
+    if(wasTrue)
+    {
+        return "Implentation of \"if\"";
+    }
+    else 
+    {
+        return "Implementation of \"else\"";
+    }
+}
+console.log("Evaluation of if-else ,value passed is true "+isTrue(true));
+console.log("Evaluation of if-else ,value passed is false "+isTrue(false));
